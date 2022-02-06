b719fcf52b352f1e6dcd008f3c2393139e5cf8c3
yutiborn <yutib@webschool.co.il>
2022-02-06 21:25:52 +0200

    numbers

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,10 +3,34 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div>
-        Welcome
+        
       
     </div>
   )
 }
 
 export default Welcome
+
+import propTypes from 'prop-types'
+
+
+const name = 'David'
+
+
+const Welcome = ({name, dodo}) => {
+    return (
+        <div>
+            Welcome, {name} + {dodo}
+        </div>
+    )
+}
+Welcome.defaultProps = {
+    name: 0
+}
+Welcome.propTypes = {
+    name: propTypes.string
+}
+
+export {Welcome} 
+
+export let firstName = 'Yossefi';
